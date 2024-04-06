c97f48a7b8b36d6f3a580c5708d7176b76ef4e1a
Eunit <eunithelp@gmail.com>
2024-04-06 17:30:42 +0100

    Feat: Add new pages, all returning same dashboard

---
+++ src/pages/explore.tsx
@@ -0,0 +1,10 @@
+import Dashboard from '@/components/dashboard/Dashboard'
+import React from 'react'
+
+export default function ExplorePage() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  )
+}
